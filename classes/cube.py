1e3698c2ca123bb7dcf73cc9b030d7182d885518
Tim Adler <tim@emrys-merlin.de>
2017-06-19 16:47:42 +0200

    Write constructor and get_color

--- classes/cube.py
+++ classes/cube.py
@@ -1,18 +1,33 @@
+from PIL import Image, ImageDraw
+import numpy as np
+
 class Cube:
-  def __init__(self,img):
-    image = img
+  def __init__(self, img):
+    self.outSize = min(img.height, img.width)
+    self.image = img.crop([0, 0, self.outSize, self.outSize])
+    self.pixels = self.image.load()
+
+  def generateImage(self, height):
+    return
+
+  def coordinateTransformation(self, x):
+    return
+
+  def getPtonSphere(self, x):
+    return
+
+  def getCubeCoordinate(slef, x):
+    return
+
+  def transfromToSquare(self, x):
+    return (x + 1) * self.outSize / 2
   
-  def generateImage():
-    return
-    
-  def coordinateTransformation(x):
-    return
-    
-  def getPtonSphere(x):
-    return
-    
-  def getCubeCoordinate(x):
-    return
-  
-  def getColor(x):
-    return
+  def getColor(self, x):
+    for i in range(len(x)):
+      if x[i] == 1:
+        del x[i]
+        break
+
+    y,z = self.transformToSquare(x)
+
+    return self.pixels[y,z]

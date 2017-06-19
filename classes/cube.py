655fcd859f04ab9501a50365c9387efe6ef1f679
Sven Grützmacher <sven@endstage.net>
2017-06-19 16:54:28 +0200

    coord-transform and getPtonSphere

--- classes/cube.py
+++ classes/cube.py
@@ -1,15 +1,25 @@
+import numpy as np
+
 class Cube:
   def __init__(self,img):
-    image = img
+    self.image = img
+    self.outSize = 10.0
   
   def generateImage():
     return
     
-  def coordinateTransformation(x):
-    return
+  def coordinateTransformation(self,x):
+    squish = x * ( 4.0 / self.outSize )
+    return (squish + np.array([-2] * len(x)))
     
   def getPtonSphere(x):
-    return
+    if not np.any(x): #if zero vector
+      ret = np.zeros(len(x)+1)
+      ret[-1] = 1
+      return ret
+    norm = np.linalg.norm(x)
+    ret = x * np.array([( 2.0 / norm**2 ) + 1] * len(x))
+    return np.concatenate((ret, [ ( ( 1.0 / norm**2 ) - 1 ) / ( ( 1.0 / norm**2 ) + 1 ) ] ))
     
   def getCubeCoordinate(x):
     return
--- main.py
+++ main.py
@@ -8,3 +8,10 @@ import progressbar
 from classes.cube import Cube
 
 print("Willkommen")
+
+mCube = Cube("hi")
+
+testVec = np.array([4,4])
+print(testVec)
+
+print(mCube.coordinateTransformation(testVec))

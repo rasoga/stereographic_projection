c9f010297e985c3cb464f1756318b1c8f75369d3
Tim Adler <tim@emrys-merlin.de>
2017-06-19 17:07:00 +0200

    Fix Typo

--- classes/cube.py
+++ classes/cube.py
@@ -24,7 +24,7 @@ class Cube:
     return np.concatenate((ret, [ ( ( 1.0 / norm**2 ) - 1 ) / ( ( 1.0 / norm**2 ) + 1 ) ] ))
     
   def getCubeCoordinate(self, x):
-    return x / np.maximum(x)
+    return x / np.maximum(np.abs(x))
 
   def transfromToSquare(self, x):
     return (x + 1) * self.outSize / 2
